5889405452301b37d6e4f5858375ac59d6b35b31
msmahimtraders95-star <ms.mahim.traders.95@gmail.com>
2026-09-02 12:39:45 +0600

    Create app_colors.dart

---
+++ lib/theme/app_colors.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  AppColors._();
+
+  static const Color primary = Color(0xFF1877F2);
+  static const Color primaryDark = Color(0xFF0D5CC2);
+
+  static const Color background = Color(0xFFF0F2F5);
+  static const Color surface = Colors.white;
+
+  static const Color textPrimary = Color(0xFF1C1E21);
+  static const Color textSecondary = Color(0xFF65676B);
+
+  static const Color border = Color(0xFFDADDE1);
+
+  static const Color success = Color(0xFF31A24C);
+  static const Color danger = Color(0xFFE41E3F);
+  static const Color warning = Color(0xFFF7B928);
+
+  static const Color like = Color(0xFF1877F2);
+  static const Color comment = Color(0xFF65676B);
+  static const Color share = Color(0xFF31A24C);
+}
